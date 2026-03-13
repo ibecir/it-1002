82a64ddcdc2ab08c079d336f1f3cfbe7d6333581
ibecir <becir@bioptimizers.com>
2026-03-13 13:44:18 +0100

    week3 - lectures

---
+++ src/main/java/ba/edu/ibu/week3/datastructures/DataStructures.java
@@ -0,0 +1,47 @@
+package ba.edu.ibu.week3.datastructures;
+
+import ba.edu.ibu.week3.enums.Gender;
+import ba.edu.ibu.week3.model.User;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class DataStructures {
+    private List<User> users;
+
+    public DataStructures() {
+        users = Arrays.asList(
+                new User("Becir", Gender.MALE, 29),
+                new User("Fatima", Gender.FEMALE, 105),
+                new User("Adnan", Gender.MALE, 35));
+    }
+
+    public List<User> getAll() {
+        return this.users;
+    }
+
+    public Optional<User> getByName(String name) {
+        return users.
+                stream().
+                filter(user -> user.getName().equals(name)).
+                findFirst();
+    }
+
+    public List<User> findAgeGreaterThan(int age) {
+        return users
+                .stream()
+                .filter(user -> age >= user.getAge())
+                .collect(Collectors.toList());
+    }
+
+    public void getByGender(Gender gender) {
+        users = users.stream()
+                .filter(user -> user.getGender().equals(gender))
+                .collect(Collectors.toList());
+        for (User u : users){
+            System.out.println(u.getName());
+        }
+    }
+}
---
+++ src/main/java/ba/edu/ibu/week3/enums/Gender.java
@@ -0,0 +1,5 @@
+package ba.edu.ibu.week3.enums;
+
+public enum Gender {
+    MALE, FEMALE
+}
