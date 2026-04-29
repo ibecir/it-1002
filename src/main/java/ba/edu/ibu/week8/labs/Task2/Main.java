ca0aeafc00a2436f8f4f1028030b78e6074578b3
Mirza <krupicmirza99@gmail.com>
2026-04-29 14:05:51 +0200

    week 7 lab solutions

---
+++ src/main/java/ba/edu/ibu/week8/labs/Task2/Main.java
@@ -0,0 +1,11 @@
+package ba.edu.ibu.week8.labs.Task2;
+
+public class Main {
+    public static void main(String[] args) {
+        Product<Double> p1 = new Product<>("Laptop", 1200.00);
+        Product<Double> p2 = new Product<>("Tablet", 850.00);
+
+        String result = p1.comparePrice(p2);
+        System.out.println(result);
+    }
+}
---
+++ src/main/java/ba/edu/ibu/week8/labs/Task3/Elephant.java
@@ -0,0 +1,17 @@
+package ba.edu.ibu.week8.labs.Task3;
+
+public class Elephant extends Animal implements Comparable<Elephant> {
+    public Elephant(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void speak() {
+        System.out.println(getName() + " trumpets!");
+    }
+
+    @Override
+    public int compareTo(Elephant other) {
+        return Integer.compare(this.getAge(), other.getAge());
+    }
+}
